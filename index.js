c8d0fa9498030d264708de53dc93493ed77a9f12
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-18 00:14:01 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function addPlayer(event) {
+
+    event.preventDefault()
+
+    const playerList = document.grtElementById("#playerData")
+
+    const firstName = document.querySelector("#fist_name");
+    const LastName = document.querySelector("#last_name");
+    const countryName = document.querySelector("#countery");
+    const score = document.querySelector("#score");
+}
